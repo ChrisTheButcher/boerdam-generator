3f6939e80d78fa3f2d5d7c69038534c8cad2d7fa
ChrisTheButcher <ccwijnia@gmail.com>
2015-07-25 17:30:24 +0200

    Using ecmascript 2015

--- Assets/src/Js/controllers/HelloController.js
+++ Assets/src/Js/controllers/HelloController.js
@@ -1,5 +1,7 @@
-app.controller('HelloController', ['$scope', function($scope) {
+app.controller('HelloController', $scope => {
   $scope.hello = 'Hello World!';
+  $scope.babelTest = (...values) => `Babel.js is working ${values}`;
 
-  console.log($scope.hello);
-}]);
+  console.log($scope.hello);  
+  console.log($scope.babelTest(1,2,3));
+});
